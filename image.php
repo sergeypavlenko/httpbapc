b3a762d58668caa58c5dcddb0bf42491749fbebc
Sergey Pavlenko <sergeypavlenko@1343804.no-reply.drupal.org>
2014-10-07 20:48:57 +0300

    Image file.

---
+++ image.php
@@ -0,0 +1,22 @@
+<?php
+
+define('IMAGE_CAMERA_NAME', 'image_camera_cache.jpeg');
+
+require 'httpbapc.php';
+
+$config = require 'config.php';
+
+header('Content-Type: image/jpeg');
+
+if (isset($_GET['old'])) {
+  echo file_get_contents(IMAGE_CAMERA_NAME);
+}
+
+$http = new Httpbapc($config['url'], $config['username'], $config['password']);
+$image_string = $http->execute();
+
+if ($image_string) {
+  file_put_contents(IMAGE_CAMERA_NAME, $image_string);
+}
+
+echo file_get_contents(IMAGE_CAMERA_NAME);
